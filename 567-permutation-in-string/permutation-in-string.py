8fa671a54f1f7451f8894f49fc967edcfcd4ab20
Mahider_n <125821991+Mahider-n@users.noreply.github.com>
2024-11-18 20:37:17 +0300

    Time: 15 ms (86.41%) | Memory: 16.9 MB (9.18%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            
+            return False
+
+        cache1 = [0] * 26
+        cache2 = [0] * 26
+
+        for i in range(len(s1)):
+
+            cache1[ord(s1[i]) - ord('a')] += 1
+            cache2[ord(s2[i]) - ord('a')] += 1
+
+        if cache1 == cache2:
+            return True
+
+        for i in range(len(s1), len(s2)):
+
+            cache2[ord(s2[i - len(s1)]) - ord('a')] -= 1
+            cache2[ord(s2[i]) - ord('a')] += 1
+            if cache1 == cache2:
+                return True
+
+        return False
