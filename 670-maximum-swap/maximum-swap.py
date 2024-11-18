b6a87cdf3a18b073c9e87a2c36dffd1ce3200762
Mahider_n <125821991+Mahider-n@users.noreply.github.com>
2024-11-18 20:55:37 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (32.05%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        n = str(num)
+        l = list(n)
+        length = len(l)
+        
+        # Iterate through the list
+        for i in range(length):
+            # Only proceed if there are elements after l[i]
+            if i + 1 < length and l[i] < max(l[i+1:]):
+                # Find the max number in the rest of the list
+                m = max(l[i+1:])
+                # Find the index of that max number, in the context of the whole list
+                idx = len(l) - 1 - l[::-1].index(m)
+                # Swap the values
+                l[i], l[idx] = l[idx], l[i]
+                break
+        
+        # Return the modified list as an integer
+        return int(''.join(l))
