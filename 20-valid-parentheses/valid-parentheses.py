4d98b8892f208949968b70aa5f12b32c103171ae
Mahider_n <125821991+Mahider-n@users.noreply.github.com>
2024-12-23 18:11:52 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (5.58%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        char={")":"(","]":"[","}":"{"}
+        for c in s:
+            if c in char:
+                if stack and stack[-1]==char[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+ 
+        
